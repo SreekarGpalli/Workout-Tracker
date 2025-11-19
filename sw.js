da1c300b48b0a14bf1b15bec91c302ad4e504e56
Sreekar Gownivaripalli <147900841+SreekarGpalli@users.noreply.github.com>
2025-11-19 18:32:36 -0500

    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'iron-log-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    })
+  );
+});
